f2a039f9e06ac1c80f785d645d990380701b2f71
VishavjeetS <vishavhanspal00@gmail.com>
2023-11-04 21:29:23 +0530

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,12 @@ import 'package:flutter_catalog/pages/login_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
